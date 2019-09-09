b2762362bf4dd7cb6607dc4c1e8560f2d8e09f7e
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-09 07:08:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,16 +8,23 @@ def nyc_pigeon_organizer(data)
         end
       new_hash[name][:color].push("#{key}")
     end
-    new_hash
   end
   gender_hash = data[:gender]
-      gender_hash.each do |key, value|
-      value.each do |name|
-        if new_hash.key? (name) == false
+  gender_hash.each do |key, value|
+    value.each do |name|
+      if new_hash.key? (name) == false
         new_hash[name] = {:color => [], :gender => [], :lives => []}
-        end
+      end
       new_hash[name][:gender].push("#{key}")
     end
-    new_hash
   end
+  lives_hash = data[:lives]
+  lives_hash.each do |key, value|
+    value.each do |name|
+      if new_hash.key? (name) == false
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+      new_hash[name][:lives].push("#{key}")
+    end
+  
 end
