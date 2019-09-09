34b8b66260ec8d5b44da6819a4236ddbd992646c
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-09 06:45:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,13 @@ def nyc_pigeon_organizer(data)
   new_hash = Hash.new
   color_hash = data[:color]
     color_hash.each do |key, value|
-      if new_hash.key? (value[i])
-        new_hash[value[i]][:color] = "key"
+      value.each do |name|
+      if new_hash.key? (name)
+        new_hash[name][:color].push("#{key}")
       else
-        new_hash[value[i]] = 
+        new_hash[value[i]] = {:color, :gender, :lives}
+      end
+      new_hash
+  gender_hash
+      
 end
